e0c10bab406b05967b62b0268b005e5e6d8fb74a
jose <90952627+joseenricoaquino@users.noreply.github.com>
2025-10-23 06:24:25 +0800

    done challenge 2 header parser

--- index.js
+++ index.js
@@ -20,9 +20,20 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/hello',  (req, res) => {
+  res.json({ greeting: 'hello API' })
 });
+
+app.get('/api/whoami', (req, res) => {
+        const ipreq = req.ip
+        const lang = req.headers['accept-language']
+        const softw = req.headers['user-agent']
+
+  res.json({ ipaddress: ipreq, language: lang, software: softw })
+});
+
+
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
